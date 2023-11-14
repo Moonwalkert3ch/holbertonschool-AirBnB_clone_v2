9a48f5904bc44c9573761dcd0c6c21d0ed6306d4
Moonwalkert3ch <6620@holbertonstudents.com>
2023-11-14 14:05:26 -0600

    fixed typo

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 
 
 @app.route("/", strict_slashes=False)
-def hello():
+def hello_hbnb():
     return "Hello HBNB!"
 
 
@@ -42,7 +42,7 @@ def number_template(n):
 
 @app.route("/number_odd_or_even/<int:n>", strict_slashes=False)
 def number_odd_or_even(n):
-    return render_template("6-number.html", n=n)
+    return render_template("6-number_odd_or_even.html", n=n)
 
 
 if __name__ == "__main__":
