317819e70afb5f514861ac612b8febf665c7624f
Moonwalkert3ch <6620@holbertonstudents.com>
2023-11-13 23:42:36 -0600

    solve pep8

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -18,8 +18,8 @@ def hbnb():
 
 @app.route("/c/<text>", strict_slashes=False)
 def c(text):
-        text = text.replace("_", " ")
-        return "C {}".format(text)
+    text = text.replace("_", " ")
+    return "C {}".format(text)
 
 
 @app.route("/python", strict_slashes=False)
@@ -31,12 +31,12 @@ def python(text="is cool"):
 
 @app.route("/number/<int:n>", strict_slashes=False)
 def number(n):
-     return "{} is a number".format(n)
+    return "{} is a number".format(n)
 
 
 @app.route("/number_template/<int:n>", strict_slashes=False)
 def number_template(n):
-     return render_template("5-number.html", n=n)
+    return render_template("5-number.html", n=n)
 
 
 if __name__ == "__main__":
