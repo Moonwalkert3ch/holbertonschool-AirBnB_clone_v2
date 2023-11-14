694b4dbb58ea174e92f6692c4cbcb1268262fe34
Moonwalkert3ch <6620@holbertonstudents.com>
2023-11-14 14:41:21 -0600

    working through errors

--- models/state.py
+++ models/state.py
@@ -1,8 +1,25 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
-from models.base_model import BaseModel
+"""Defines the State class"""
+import models
+from os import getenv
+from models.base_model import BaseModel, Base
+from models.city import City
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 
-class State(BaseModel):
-    """ State class """
-    name = ""
+class State(BaseModel, Base):
+    """ Represents State class """
+    __tablename__ = 'states'
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", backref="state", cascade="delete")
+
+    if getenv("HBNB_TYPE_STORAGE") != "db":
+        @property
+        def cities(self):
+            """Retrieve list of related cities"""
+            related_city = []
+            for cities in list(models.storage.all(City).values()):
+                if cities.state_id == self.id:
+                    related_city.append(cities)
+            return related_city
