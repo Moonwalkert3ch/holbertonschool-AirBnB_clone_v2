e6d103a1106de55215a57915a54088566c7c12aa
Moonwalkert3ch <6620@holbertonstudents.com>
2023-11-16 17:42:03 -0600

    ed

--- models/state.py
+++ models/state.py
@@ -11,10 +11,15 @@ from sqlalchemy.orm import relationship
 
 class State(BaseModel, Base):
     """Representation of state """
-    if models.storage_t == "db":
-        __tablename__ = 'states'
-        name = Column(String(128), nullable=False)
-        cities = relationship("City", backref="state")
-    else:
-        name = ""
-	cites = []
+    __tablename__ = 'states'
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", backref="state", cascade='delete')
+
+    @property
+    def cities(self):
+            """Retrieve list of related cities"""
+            related_city = []
+            for cities in list(models.storage.all(City).values()):
+                if cities.state_id == self.id:
+                    related_city.append(cities)
+            return related_city
